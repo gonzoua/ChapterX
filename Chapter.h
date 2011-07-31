7bf1c7d7181b6c1ef247b02d700176b8f32e16fe
Oleksandr Tymoshenko <gonzo@bluezbox.com>
2011-07-31 14:13:31 -0700

    Initial code drop

---
+++ Chapter.h
@@ -0,0 +1,15 @@
+//
+//  Chapter.h
+//  ChapterX
+//
+//  Created by Oleksandr Tymoshenko on 11-07-31.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Chapter : NSObject {
+
+}
+
+@end
---
+++ Chapter.m
@@ -0,0 +1,23 @@
+//
+//  Chapter.m
+//  ChapterX
+//
+//  Created by Oleksandr Tymoshenko on 11-07-31.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "Chapter.h"
+
+@implementation Chapter
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
