9d9a0bfaf26d7ea0798e419e43a77d1dd972b543
Oleksandr Tymoshenko <gonzo@bluezbox.com>
2011-07-31 15:04:01 -0700

    Create chapters array

--- ChapterX/main.m
+++ ChapterX/main.m
@@ -7,14 +7,39 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "Chapter.h"
 
 int main (int argc, const char * argv[])
 {
 
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    NSURL *xmlURL;
+    NSError *xmlError;
+    
+    if (argc < 2) {
+        fprintf(stderr, "Missing input XML file\n"); 
+        fprintf(stderr, "Usage: ChapterX file.xml\n");
+        exit(1);
+    }
+    
+    xmlURL = [NSURL fileURLWithPath:[NSString stringWithCString:argv[1] encoding:NSUTF8StringEncoding]];
 
-    // insert code here...
-    NSLog(@"Hello, World!");
+    NSXMLDocument *document = [[NSXMLDocument alloc] initWithContentsOfURL:xmlURL options:0 error:&xmlError];
+    if (!document) {
+        NSLog(@"XML parsing error: %@\n", [xmlError localizedDescription]);
+        exit(1);
+    }
+    
+    NSArray *nodes = [document nodesForXPath:@"./chapters/chapter"
+                                      error:&xmlError];
+    if (xmlError) {
+        NSLog(@"Internal XML error: %@\n", [xmlError localizedDescription]);
+        exit(1);
+    }
+    
+    for (NSXMLNode *n in nodes) {
+        Chapter *chapter = [[Chapter alloc] initWithXMLNode:n];
+    }
 
     [pool drain];
     return 0;
