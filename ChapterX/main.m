eb08271d0b0c2fb592b6c7c1c064d8b73424bc60
Oleksandr Tymoshenko <gonzo@bluezbox.com>
2011-07-31 14:03:22 -0700

    Initial Commit

---
+++ ChapterX/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ChapterX
+//
+//  Created by Oleksandr Tymoshenko on 11-07-31.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    // insert code here...
+    NSLog(@"Hello, World!");
+
+    [pool drain];
+    return 0;
+}
+
